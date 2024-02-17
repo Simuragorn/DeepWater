35d3162b11900edaabcbdfd766456b2319e6d81c
Bogdan.G <bogdan.g@solity.io>
2024-02-18 01:38:14 +0200

    Update unity editor version, add movement gears, update light system

--- Assets/Scripts/SubmarineMovement.cs
+++ Assets/Scripts/SubmarineMovement.cs
@@ -1,14 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+[Serializable]
+public class Gear
+{
+    public int Number;
+    public float Speed;
+}
 
 public class SubmarineMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rigidbody;
-    [SerializeField] float xSpeed = 10f;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] float gearDelay = 1f;
+    [SerializeField]
+    List<Gear> gears = new() {
+        new Gear { Number = -1, Speed = -0.5f },
+        new Gear { Number = 0, Speed = 0f },
+        new Gear { Number = 1, Speed = 1f },
+        new Gear { Number = 2, Speed = 1.5f },
+    };
     [SerializeField] float gravityChangingSpeed = 0.7f;
     [SerializeField] float maxGravity = 0.2f;
     [SerializeField] float minGravity = -0.2f;
 
-    [SerializeField] float xVelocityOffset = 0;
+    Gear currentGear;
+    int minGearNumber;
+    int maxGearNumber;
+    float gearDelayCounter = 0f;
+
+    private void Start()
+    {
+        currentGear = gears.FirstOrDefault(g => g.Number == 0);
+        minGearNumber = gears.Min(g => g.Number);
+        maxGearNumber = gears.Max(g => g.Number);
+    }
 
     void Update()
     {
@@ -23,15 +51,38 @@ public class SubmarineMovement : MonoBehaviour
 
     private void HorizontalMovement()
     {
-        float xAxle = Input.GetAxis("Horizontal");
-        if (true ||Input.GetAxisRaw("Horizontal") != 0)
+        HandleGear();
+
+        float xMovement = currentGear.Speed * Time.deltaTime;
+        if (!spriteRenderer.flipX)
         {
-            xVelocityOffset = xAxle;
+            xMovement *= -1;
         }
-        float xMovement = xVelocityOffset * xSpeed * Time.deltaTime;
         Vector2 oldVelocity = rigidbody.velocity;
         rigidbody.velocity = new Vector2(oldVelocity.x + xMovement, oldVelocity.y);
-        Debug.Log($"X Velocity: {rigidbody.velocity.x}");
+    }
+
+    private void HandleGear()
+    {
+        int gearOffset = -(int)Input.GetAxisRaw("Horizontal");
+        int newGearNumber = currentGear.Number + gearOffset;
+        newGearNumber = Mathf.Max(newGearNumber, minGearNumber);
+        newGearNumber = Mathf.Min(newGearNumber, maxGearNumber);
+        if (newGearNumber != currentGear.Number)
+        {
+            TryChangeGear(newGearNumber);
+        }
+        gearDelayCounter -= Time.deltaTime;
+    }
+
+    private void TryChangeGear(int newGearNumber)
+    {
+        if (gearDelayCounter <= 0)
+        {
+            currentGear = gears.FirstOrDefault(g => g.Number == newGearNumber);
+            gearDelayCounter = gearDelay;
+            Debug.Log($"Gear: {currentGear.Number}");
+        }
     }
 
     private void BuoyancyChanging()
@@ -42,6 +93,5 @@ public class SubmarineMovement : MonoBehaviour
         newGravity = Mathf.Max(newGravity, minGravity);
         newGravity = Mathf.Min(newGravity, maxGravity);
         rigidbody.gravityScale = newGravity;
-        Debug.Log($"Buoyancy: {rigidbody.gravityScale}");
     }
 }
